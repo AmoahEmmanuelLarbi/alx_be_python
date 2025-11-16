002459fbf910199083bc171caf97b0b064af14ef
Manuel <emmanuellarbiamoah@gmail.com>
2025-11-16 21:39:50 +0000

    Created a python script to perform basic arithmetic and calculate simple interest

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# variables
+number1 = 10
+number2 = 5
+
+#calculation
+#addition
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+
+#subtraction
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+
+#multiplication
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+"""
+Objective: Apply basic Python arithmetic operations and variable assignments to 
+calculate the simple interest on a given principal amount, rate of interest, and time.
+"""
+
+# formual for interest calculation : I = P * R * T
+# variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#calculate interest
+interest = principal * rate * time
+print(f"The simple interest is {interest}")
