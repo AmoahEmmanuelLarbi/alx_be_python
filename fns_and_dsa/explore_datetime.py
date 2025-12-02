3a2c76b7408e2938174025cf89c729b983ff9711
Manuel <emmanuellarbiamoah@gmail.com>
2025-12-02 12:40:24 +0000

    Modified script for datetime module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -27,5 +27,5 @@ def calculate_future_date(days_to_added):
     return future_date.strftime("%Y-%m-%d")
     
 
-number_of_days = int(input("Enter number of days to add to the current date: "))
+number_of_days = int(input("Enter the number of days to add to the current date: "))
 print(calculate_future_date(number_of_days))
