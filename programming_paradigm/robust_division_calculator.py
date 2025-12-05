4cb49be97f21217eeecebb8e658ca83d9f8ed6c7
Manuel <emmanuellarbiamoah@gmail.com>
2025-12-05 20:51:23 +0000

    Created a robust divison calculator

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,27 @@
+"""
+Docstring for alx_be_python.programming_paradigm.robust_division_calculator
+
+Objective: Implement a division calculator that robustly handles errors like division by zero and non-numeric inputs using command line arguments.
+
+Task Description:
+Create two Python scripts: robust_division_calculator.py, which contains the division logic including error handling, and main.py, which interfaces with the user through the command line.
+"""
+
+"""Define a function safe_divide(numerator, denominator) that performs division, handling potential errors:"""
+def safe_divide(numerator, denominator):
+    #handle zero divison error
+    try:
+        if denominator == 0:
+            raise ZeroDivisionError()
+        # if float(numerator):
+        #     raise ValueError("Error: Please enter numeric values only.")
+        return float(numerator) / float(denominator)
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero")
+    except ValueError as e:
+        print("Error: Please enter numeric values only")
+    
+
+
+result = safe_divide("ten", 4)
+print(result)
