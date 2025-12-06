3274d42213aa35c3d68ccf5a20aa902373adeaf0
Manuel <emmanuellarbiamoah@gmail.com>
2025-12-06 00:15:35 +0000

    Created a script to run unittest

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,36 @@
+"""
+Objective: Learn the basics of unit testing in Python by writing tests for a provided SimpleCalculator class that supports addition, subtraction, multiplication, and division operations.
+"""
+
+"""
+Create a test_simple_calculator.py script to define and run unit tests for each method in the SimpleCalculator class. 
+Your tests should cover various scenarios to ensure the class functions correctly.
+"""
+import unittest
+from simple_calculator import SimpleCalculator # custom module
+
+calculator = SimpleCalculator()
+#test class
+class TestSimpleCalculator(unittest.TestCase):
+    def test_addition(self):
+        result = calculator.add(2, 5)
+        self.assertEqual(result, 7)
+        self.assertEqual(calculator.add(3, -1), 2)
+
+    def test_subtraction(self):
+        result = calculator.subtract(5, 2)
+        self.assertEqual(result, 3)
+
+    def test_multiply(self):
+        result = calculator.multiply(2, 5)
+        self.assertEqual(result, 10)
+        self.assertEqual(calculator.multiply(4, -7), -28)
+
+    def test_divide(self):
+        result = calculator.divide(10, 2)
+        self.assertEqual(result, 5)
+        with self.assertRaises(ZeroDivisionError):
+            calculator.divide(4, 0)
+             
+if __name__ == "__main__":
+    unittest.main()
