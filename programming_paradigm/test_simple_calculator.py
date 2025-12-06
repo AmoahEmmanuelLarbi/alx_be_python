143404fb43955ca2f74bbbf4a3a8b3ed8450ba80
Manuel <emmanuellarbiamoah@gmail.com>
2025-12-06 00:41:31 +0000

    Created a script to run unittest

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,25 +12,31 @@ from simple_calculator import SimpleCalculator # custom module
 calc = SimpleCalculator()
 #test class
 class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+    
     def test_addition(self):
         result = calc.add(2, 5)
         self.assertEqual(result, 7)
-        self.assertEqual(calc.add(3, -1), 2)
+        self.assertEqual(self.calc.add(3, -1), 2)
 
     def test_subtraction(self):
         result = calc.subtract(5, 2)
         self.assertEqual(result, 3)
+        self.assertEqual(self.calc.subtract(3, 2), 1)
 
     def test_multiply(self):
         result = calc.multiply(2, 5)
         self.assertEqual(result, 10)
         self.assertEqual(calc.multiply(4, -7), -28)
+        self.assertEqual(self.calc.multiply(3, 2), 6)
 
     def test_divide(self):
         result = calc.divide(10, 2)
         self.assertEqual(result, 5)
+        self.assertEqual(self.calc.divide(4, 2), 2)
         with self.assertRaises(ZeroDivisionError):
-            calc.divide(4, 0)
+            self.calc.divide(4, 0)
              
 if __name__ == "__main__":
     unittest.main()
