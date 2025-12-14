80fd40da3d5844b6af3bd915ccee1ddd232949a4
Manuel <emmanuellarbiamoah@gmail.com>
2025-12-14 14:15:15 +0000

    Added a script implementing class methods and static methods

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,37 @@
+"""
+Docstring for alx_be_python.oop.class_static_methods_demo
+
+Create a Python script named class_static_methods_demo.py. 
+In this script, define a class Calculator that includes both a class method and a static method to perform calculations. 
+This task aims to illustrate when and how to use @classmethod and @staticmethod decorators, highlighting their differences and practical applications.
+"""
+
+class Calculator:
+    #class attribute
+    calculation_type = "Arithmetic Operations"
+
+    #constructor
+    # def __init__(self):
+    #     pass
+
+    #class method
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+def main():
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
+
+if __name__ == "__main__":
+    main()
